4c8bf731f4a30f4c7165558bd900215cb8822f84
Waleed Khalid <wak327@gmail.com>
2025-10-01 18:43:33 +0500

    Have init spawn service supervisor

--- userspace/init.c
+++ userspace/init.c
@@ -3,9 +3,16 @@
 #include "../kernel/console.h"
 #include "../kernel/scheduler.h"
 #include "../gui/gui.h"
+#include "../services/services.h"
 
 void init_process_entry(void) {
     console_write_line("[init] starting");
+
+    if (scheduler_create_process(service_init_daemon_entry, "svc-init") != 0) {
+        console_write_line("[init] failed to spawn service init");
+    } else {
+        console_write_line("[init] service init spawned");
+    }
 
     if (scheduler_create_process(shell_process_entry, "shell") != 0) {
         console_write_line("[init] failed to spawn shell");
