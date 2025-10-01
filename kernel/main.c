91e8b4d30b412a7e9c7e02a3ba5110961e33702d
Waleed Khalid <wak327@gmail.com>
2025-10-01 18:10:32 +0500

    Integrate driver stack into kernel startup

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,12 @@
 #include "memory.h"
 #include "scheduler.h"
 
+#include "../drivers/display.h"
+#include "../drivers/keyboard.h"
+#include "../drivers/network.h"
+#include "../drivers/storage.h"
+
+#include <stddef.h>
 #include <stdint.h>
 
 extern void rust_entry(void);
@@ -9,7 +15,25 @@ extern void rust_entry(void);
 static void task_alpha(void);
 static void task_beta(void);
 
+static void pump_keyboard(void) {
+    keyboard_poll();
+    while (keyboard_available()) {
+        char ch = keyboard_read_char();
+        char msg[32] = "[kbd] key=";
+        size_t len = 0;
+        while (msg[len] != '\0') {
+            ++len;
+        }
+        msg[len++] = ch ? ch : '?';
+        msg[len] = '\0';
+        console_write_line(msg);
+    }
+}
+
 void kernel_main(void) {
+    display_init();
+    display_write_text("jinOs display online", 0, 0, 0x1E);
+
     console_init();
     console_write_line("jinOs Kernel Started");
 
@@ -18,6 +42,12 @@ void kernel_main(void) {
 
     paging_init();
     heap_init();
+
+    keyboard_init();
+    storage_init();
+    network_init();
+
+    display_write_text("Drivers initialised", 1, 0, 0x1A);
 
     scheduler_init();
     if (scheduler_create_process(task_alpha, "task-A") != 0) {
@@ -43,6 +73,7 @@ void kernel_main(void) {
 static void task_alpha(void) {
     console_write_line("[task-A] initialized");
     for (int i = 0; i < 3; ++i) {
+        pump_keyboard();
         console_write_line("[task-A] yielding control");
         scheduler_yield();
     }
@@ -52,6 +83,24 @@ static void task_alpha(void) {
 
 static void task_beta(void) {
     console_write_line("[task-B] initialized");
+
+    uint16_t *sector = (uint16_t *)kmalloc(512);
+    if (sector) {
+        for (int i = 0; i < 256; ++i) {
+            sector[i] = (uint16_t)(0xCA00 | i);
+        }
+        ata_write_sector(1, sector);
+        ata_read_sector(1, sector);
+    }
+
+    uint8_t packet[32];
+    for (size_t i = 0; i < sizeof(packet); ++i) {
+        packet[i] = (uint8_t)i;
+    }
+    network_send_packet(packet, sizeof(packet));
+    uint8_t recv_buffer[32];
+    network_receive_packet(recv_buffer, sizeof(recv_buffer));
+
     for (int i = 0; i < 3; ++i) {
         console_write_line("[task-B] running workload");
         scheduler_yield();
